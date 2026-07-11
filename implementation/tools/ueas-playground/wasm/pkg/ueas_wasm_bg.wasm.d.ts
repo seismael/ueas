04bf8de28cebf7c5a1a12941678dfd39beac486d
seismael <firas.ismael@gmail.com>
2026-07-11 20:59:20 +0300

    chore: force commit WASM pkg for Vercel deployment

---
+++ implementation/tools/ueas-playground/wasm/pkg/ueas_wasm_bg.wasm.d.ts
@@ -0,0 +1,17 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const execute_ueas: (a: number, b: number) => [number, number, number, number];
+export const parse_ueas: (a: number, b: number) => [number, number, number, number];
+export const profile_hardware: (a: number, b: number) => [number, number, number, number];
+export const transpile_ueas: (a: number, b: number, c: number, d: number) => [number, number, number, number];
+export const verify_crypto: (a: number, b: number) => [number, number, number, number];
+export const main: () => void;
+export const profile_memory: (a: number, b: number) => [number, number, number, number];
+export const profile_complexity: (a: number, b: number) => [number, number, number, number];
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __externref_table_dealloc: (a: number) => void;
+export const __wbindgen_start: () => void;
